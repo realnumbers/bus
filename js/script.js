8464971fef19700238fe8acb1b73580e42c51149
Julian Sparber <julian@sparber.net>
2014-01-19 14:47:52 +0100

    searchs only for one language names

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
+var lang = "it";
 var busstops = $.getJSON( "js/busstops.json", function(data){
 		console.log("success");
 		console.log(data);
 		autocom(data);
 	});
 //console.log(busstops);
+if ( navigator.language === "de" ){
+		lang = de;
+}
+
 function autocom(data){
 $(function() {
 	$( "#to-input" ).autocomplete({
-		source: data
+		source: data[lang]
 	});
 });
-}
+ }
