fc29ce20c5e37fd760181756d57f0ad26fd99b15
Julian Sparber <julian@sparber.net>
2014-01-22 00:50:43 +0100

    rewrote script.js not finished yet

--- js/script.js
+++ js/script.js
@@ -1,28 +1,22 @@
-var data_ready = false;
+var SECTION = document.getElementsByClassName("js-section");
+var current_section = 0;
 var lang = "it";
-var selected_option = 0;
 var selected_busstop = new Array(5);
 var busstops = $.getJSON( "js/busstops.json", function(data){
-		//console.log("success");
-		//console.log(data);
-		data_ready = true;
 		autocom();
 	});
-//console.log(busstops);
-var i = 1;
-while ( i < 2 ){
-	document.getElementById("input-"+i).style.display = "none";
-	i++;
-}
-i = selected_option +1;
-while ( i < 2 ){
-	document.getElementsByClassName("section-input")[i].style.display = "none";
-	i++;
-}
 if ( navigator.language === "de" ){
 		lang = de;
 }
+clearPage();
 
+function clearPage(){
+	var i = 1;
+	while ( i < SECTION.length ){
+			SECTION[i].style.display = "none";
+			i++;
+	}
+}
 /*$.ajax({
 		dataType: "jsonp",
 		jsonpCallback: 'jsonCallback',
@@ -34,45 +28,47 @@ if ( navigator.language === "de" ){
 
 
 function autocom(){
-		var i = 0;
-		var value = document.getElementsByClassName("input-element input-"+selected_option)[0].value;
-		var value_list = value.split(' ');
-		console.log(selected_option);
-		document.getElementsByClassName("suggest-element input-"+selected_option)[0].innerHTML = "";
-		document.getElementsByClassName("suggest-element input-"+selected_option)[1].innerHTML = "";
-		document.getElementsByClassName("suggest-element input-"+selected_option)[2].innerHTML = "";
-		document.getElementsByClassName("suggest-element input-"+selected_option)[3].innerHTML = "";
-		document.getElementsByClassName("suggest-element input-"+selected_option)[4].innerHTML = "";
-		data = busstops.responseJSON;
-		//console.log("Change");
-		function tryMatch(element, index, array) {
-		var respons = value_list.every(tryPattern);
-			if ( i < 5 && respons){
-				selected_busstop[i] = element;
-				document.getElementsByClassName("suggest-element input-"+selected_option)[i].innerHTML = "<p class=\"line-big list\">"+element.name+",</p> <p class=\"line-small list\">"+element.city+"</p>";
-				i++;
-			}
-			if ( i > 4 )
-					return false;
-		return true;
-			
-		function tryPattern(item, index, array){
-			//console.log(item);
-			//console.log(element);
-			var pattern = new RegExp(item, "i");
-			var found = element.label.match(pattern);
-			if ( value != "" && found != null){
-				return true;
-			}	
-			else{
-				return false;
-			}
-		}
-			}
+	var i;
+	var children = SECTION[current_section].children;
+	var input_string = children[1].value;
+	input_string = input_string.split(' ');
 
-		var str = data[lang];
-		str.every(tryMatch);
- }
+	i = 2
+	while ( i < children.length ){
+			children[i].innerHTML = "";
+			i++;
+	}
+		
+	console.log("Input Changed");
+	data = busstops.responseJSON;
+	i = 2;
+	data[lang].every( function (element, index, array){
+			var res = input_string.every( function (item, index, array){
+					var pattern = new RegExp(item, "i");
+					var found_match = element.label.match(pattern);
+
+					if ( found_match != null ){
+							return true;
+					}
+					else{
+							return false;
+					}
+
+			});
+
+			if ( res && i < children.length ){
+
+					console.log(element);
+					children[i].innerHTML = element.name+", "+element.city;
+					i++;
+			}
+			if ( i >= children.length )
+					return false;
+			else
+					return true;
+
+});
+}
  function selectBusstop(div_number){
 	document.getElementsByClassName("line-big")[selected_option].innerHTML = selected_busstop[div_number].name+", ";
 	document.getElementsByClassName("line-small")[selected_option].innerHTML = selected_busstop[div_number].city;
