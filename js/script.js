d0c8e502d405eeb95e4305d8bf23c3f48672f669
Julian Sparber <julian@sparber.net>
2014-01-23 20:45:59 +0100

    refactoring js/script.js

--- js/script.js
+++ js/script.js
@@ -3,45 +3,43 @@ var lang = "it";
 var form_stop;
 var to_stop;
 var matching_busstops = new Array(5);
-var busstops = $.getJSON( "js/busstops.json", function(data){
+var busstops = $.getJSON( "js/busstops.json", function(data) {
 		autocom(0);
 	});
-if ( navigator.language === "de" ){
+if (navigator.language === "de") {
 		lang = de;
 }
 clearPage();
 
-function clearPage(){
+function clearPage() {
 	var i = 1;
-	while ( i < SECTION.length ){
-			SECTION[i].style.display = "none";
-			i++;
+	while (i < SECTION.length) {
+		SECTION[i].style.display = "none";
+		i++;
 	}
 }
 
-
-
-function autocom(current_section){
+function autocom(current_section) {
 	var i;
 	var children = SECTION[current_section].children;
 	var input_string = children[1].value;
 
 	i = 2
-	while ( i < children.length ){
+	while (i < children.length) {
 			children[i].children[0].innerHTML = "";
 			children[i].children[1].innerHTML = "";
 			i++;
 	}
-	if ( input_string != "" ){
+	if (input_string != "") {
 	input_string = input_string.split(" ");
 	data = busstops.responseJSON;
 	i = 2;
-	data[lang].every( function (element, index, array){
-			var res = input_string.every( function (item, index, array){
+	data[lang].every( function (element, index, array) {
+			var res = input_string.every( function (item, index, array) {
 					var pattern = new RegExp(item, "i");
 					var found_match = element.label.match(pattern);
 
-					if ( found_match != null ){
+					if (found_match != null) {
 							return true;
 					}
 					else{
@@ -50,14 +48,14 @@ function autocom(current_section){
 
 			});
 
-			if ( res && i < children.length ){
+			if (res && i < children.length) {
 
 					matching_busstops[i-2] = element;
 					children[i].children[0].innerHTML = element.name+", ";
 					children[i].children[1].innerHTML = element.city;
 					i++;
 			}
-			if ( i >= children.length ){
+			if (i >= children.length) {
 					return false;
 			}
 			else{
@@ -67,48 +65,48 @@ function autocom(current_section){
 });
 }
 }
-function selectBusstop(current_section, div_number){
-	SECTION[current_section].children[0].children[1].innerHTML = matching_busstops[div_number].name+", ";
+function selectBusstop(current_section, div_number) {
+	SECTION[current_section].children[0].children[1].innerHTML = matching_busstops[div_number].name + ", ";
 	SECTION[current_section].children[0].children[2].innerHTML = matching_busstops[div_number].city;
 	SECTION[current_section].children[1].value = "";
 	makeBlank(current_section);
-	unBlank(current_section+1);
+	unBlank(current_section + 1);
 	autocom(current_section);
-	if ( current_section === 0 ){
+	if (current_section === 0) {
 			from_stop = matching_busstops[div_number].id;
 	}
-	if ( current_section === 1 ){
+	if (current_section === 1) {
 			to_stop = matching_busstops[div_number].id;
 	}
 }
-function makeBlank(current_section){
+function makeBlank(current_section) {
 	var i = 1;
-	while ( i < SECTION[current_section].children.length ){
+	while (i < SECTION[current_section].children.length) {
 			SECTION[current_section].children[i].style.display = "none";
 			i++;
 	}
 }
-function unBlank(current_section){
+function unBlank(current_section) {
 	var i = 0;
-	while ( i < SECTION.length ){
+	while (i < SECTION.length) {
 			makeBlank(i);
 			i++;
 	}
 	SECTION[current_section].style.display = "block";
 	i = 0;
-	while ( i < SECTION[current_section].children.length ){
+	while (i < SECTION[current_section].children.length) {
 			SECTION[current_section].children[i].style.display = "block";
 			i++;
 	}
 }
-function selectTime(current_section){
+function selectTime(current_section) {
 		var data = SECTION[current_section].children[1].value;
 		var time = SECTION[current_section].children[2].value;
 		SECTION[current_section].children[0].children[1].innerHTML = data + ", ";
 		SECTION[current_section].children[0].children[2].innerHTML = time;
 		getRoute(data,time);
 }
-function getRoute(data, time){
+function getRoute(data, time) {
 	data = data.replace(/\//g, ":");
 	data = data.split(":");
 	console.log(data);
@@ -119,9 +117,9 @@ function getRoute(data, time){
 	url = url + "&yyyymmddhhmm=" + data[2] + data[1] + data[0] + time;
 $.ajax({
 		dataType: "jsonp",
-		jsonpCallback: 'jsonCallback',
+		jsonpCallback: "Callback",
 		url: url,
-	 	success: function(data){
+	 	success: function(data) {
 				console.log(data);
 		}
 });
