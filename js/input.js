28612cd5b96b263822f48485bef2e45bce5ec2f0
Julian Sparber <julian@sparber.net>
2014-04-04 21:17:38 +0200

    [refactor] cleaned input.js

--- js/input.js
+++ js/input.js
@@ -3,18 +3,8 @@ var matchingBusstops = new Array(5);
 removeClickDelay();
 onEnterEvent();
 
-function onEnterEvent() {
-	$("#date-input").keydown(function(event){
-		if(event.keyCode == 13)
-			submitTime();	
-	});
-	$("#time-input").keydown(function(event){
-		if(event.keyCode == 13)
-			submitTime();
-	});
-}
-
 function busstopInput(el) {
+	console.log(el);
 	var inputString = $(el).val();
 	var i = 0;	
 	if (inputString != "") {
@@ -53,277 +43,67 @@ function showMatchMsg(){
 function hideMatchMsg(){
 }
 
-function selectBusstop(item, resultNumber) {
-	var tmpObj = new Object();
-	tmpObj = History.getState().data;
-	if (item == "from") {
-		tmpObj.dep = matching_busstops[resultNumber].id;
-		tmpObj.citydep = matching_busstops[resultNumber].city;
-		tmpObj.namedep = matching_busstops[resultNumber].name;
-	}
-	else {
-		tmpObj.arr = matching_busstops[resultNumber].id;
-		tmpObj.cityarr = matching_busstops[resultNumber].city;
-		tmpObj.namearr = matching_busstops[resultNumber].name;
-	}
-	replaceUrl(tmpObj);
-	$(".js-active").find(".js-name:first").text(matching_busstops[resultNumber].name + ", ");
-	$(".js-active").find(".js-city:first").text(matching_busstops[resultNumber].city);
-	$(".js-active").find(":input").val("");	
-	console.log(matching_busstops);
-	//$(".js-active").find(".cancel-input").hide();
-	//hideElement(".js-input");
-	$(".js-active-input").find(".cancel-input").hide();
-	$(".js-active").find(".collapse").slideToggle(200, hideCollapsedContent);
-	activateNextSection();
+function selectBusstop(el) {
+	console.log($(el));
 }
 
 function autoSetTime() {
-	if (History.getState().data.time == undefined || History.getState().data.date == undefined) {
 		var currentdate = new Date();
 		var day = addZero(currentdate.getDate());
 		var month = addZero(currentdate.getMonth() + 1);
 		var year = currentdate.getFullYear();
 		var hours = addZero(currentdate.getHours());
 		var minutes = addZero(currentdate.getMinutes());
-		$(".date").val(day + "." + month + "."  + year);
-		$(".time").val(hours + ":" + minutes);
-	}
-	else {
-		$(".date").val(History.getState().data.date);
-		$(".time").val(History.getState().data.time);
-	}
-
-	selectTime();
 }
 function selectTime() {
 	var date = $(".date").val();
 	var time = $(".time").val();
-	var dataValid = false;
-	var tmpObj = History.getState().data;
-	var dateArray = date.split(/[\.\/\-,;:]/);
-	var timeArray = time.split(/\D/);
-	if ($("#date-input")[0].validity.valid && $("#time-input")[0].validity.valid && date != "" && time != "") { 
-		var correctDateArray = formatDate(dateArray);
-		var day = addZero(correctDateArray[0]);
-		var month = addZero(correctDateArray[1]);
-		var year = correctDateArray[2];
-	
-		var correctTimeArray = formatTime(timeArray);
-		var hours = correctTimeArray[0];
-		var minutes = correctTimeArray[1];
-
-		tmpObj.time = hours + ":" + minutes;
-		tmpObj.date = day + "." + month + "." + year;
-		replaceUrl(tmpObj);
-		$(".js-active").find(".js-name").text(day + "." + month + "." + year + ", ")
-		$(".js-active").find(".js-city").text(hours + ":" + minutes);
-		//hideElement(".js-input");
-		dataValid = true;
-	}
-	return dataValid;
-}
-function submitTime() {
-	var dataValid;
-	dataValid = selectTime();
-	if (dataValid) {
-		$(".js-active-input").find(".cancel-input").hide();
-		$(".js-active").find(".collapse").slideToggle(300);
-		activateNextSection();
-	}
-}
-function showRoute() {
-	var routeData = getRouteData();
-	hideElement(".js-suggest");
-	changeWorkElement("reset");
-	if (routeData != null) {
-		for (var i = 0; i < routeData.length; i++) {
-				if (routeData[i] != null)
-					loadOverview(routeData[i].overview);
-		}
-		$(".js-active").find(".js-suggest").show();
-	}
-	else {
-		alert("Noconnection");
-	}
-	showElement(".js-time");
-	showElement(".js-duration");
-	hideElement(".spinner");
-}
-
-function loadOverview(data) {
-	$(".js-work").find(".js-time").text(data.depTime + " - " + data.arrTime);
-	$(".js-work").find(".js-duration").text(data.duration);
-	changeWorkElement();
-}
-
-function showDetails(resultNumber) {
-	var tmpData = History.getState().data;
-	tmpData.detail = resultNumber + 1;
-	updateUrl(tmpData);
-	visibleView = "details";
-}
-function changeToDetails(resultNumber) {
-	var routeData = getRouteData()[resultNumber].connection;
-	// jump to the details section
-	$(".js-active").removeClass("js-active");
-	$("#details").find(".js-section:first").addClass("js-active");
-	$(".js-section").removeClass("active-section"); 
-	$(".js-active").addClass("active-section"); 
-	//hideElement(".js-section");
-	showElement(".js-active");
-	changeWorkElement("reset");
-	//hideElement(".js-suggest");
-
-	for (var i = 0; i < routeData.length; i++)
-		genDetailElement(routeData[i]);
-	showDetailsSection();
-}
-function genDetailElement(routeData) {
-	if (routeData.depTime == undefined) {
-		changeWorkElement();
-		showElement(".js-work").find("p").text(routeData.waitTime);
-		changeWorkElement();
-	}
-	else {
-		showElement(".js-work").find(".js-time:first").text(routeData.depTime);
-		showElement(".js-work").find(".js-time:last").text(routeData.arrTime);
-		showElement(".js-work").find(".js-name:first").text(routeData.depBusstop[1] + ", ");
-		showElement(".js-work").find(".js-name:last").text(routeData.arrBusstop[1] + ", ");
-		showElement(".js-work").find(".js-city:first").text(routeData.depBusstop[0]);
-		showElement(".js-work").find(".js-city:last").text(routeData.arrBusstop[0]);
-		showElement(".js-work").find(".js-lineNo").text("Line " + routeData.lineNo);
-
-		changeWorkElement();
-		if (routeData.waitTime != "")
-			showElement(".js-work").find("p").text(routeData.waitTime);
-		changeWorkElement();
+	if ($("#date-input")[0].validity.valid &&
+		  $("#time-input")[0].validity.valid &&
+		  date != "" &&
+		  time != ""
+		 ) {
+		date = formateDate(date.split(/[\.\/\-,;:]/));
+		alert("Time input Correct");
 	}
 }
 
-function showSearchSection() {
-	visibleView = "search";
-	//$(".js-section").removeClass("js-active active-section");
-	$("#search").find(".js-section").show();
-	//$("#search").find(".js-section:last").addClass("js-active active-section");
-	//$(".js-active").find(".js-suggest").show();
-	
-	$("#cancel").removeClass("icon-hidden-left").addClass("icon-visible");
-	$("#search").removeClass("search-hidden").addClass("search-visible");
-	$("#back").removeClass("icon-visible").addClass("icon-hidden-right");
-	$("#details").removeClass("details-visible").addClass("details-hidden");
+function addZero(number) {
+	if (number.toString().length == 1)
+		return "0" + number.toString();
+	return number;
 }
-function showDetailsSection() {
-	//$("#details").show();
-	$("#back").show();
-	//$(".js-section").removeClass("js-active active-section");
-	//$("#details").css("display", "block");
-	$("#search").find(".js-section").show();
-	$(".js-active").find(".js-suggest").show();
-	//changeWorkElement("reset");
-	//showElement("#details");
-	//showElement("#back");
-	$("#cancel").removeClass("icon-visible").addClass("icon-hidden-left");
-	$("#search").removeClass("search-visible").addClass("search-hidden");
 
-	$("#back").removeClass("icon-hidden-right").addClass("icon-visible");
-	$("#details").removeClass("details-hidden").addClass("details-visible");
-	//$("#details").removeClass("details-hidden").addClass("details-visible");
-}
-function goBack() {
-	History.back();
-	showSearchSection();
-}
-/*
-$("#details").on("transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd", function() {
-	//if (!$("#details").find(".js-section").hasClass("js-active")) {
-	//if (!$("#details").hasClass("details-visible")) {
-	if (visibleView == "search") {
-		$("#details").hide();
-		$("#back").hide();
+function formateDate(date) {
+	var dateTmp;
+	// change postion of year if it is 1st postion
+	if (date[0].toString().length == 4) {
+		dateTmp = date[2];
+		date[2] = date[0];
+		date[0] = dateTmp;
 	}
-});
-*/
-function toggleInput(element) {
-	//console.log("toggle");
-	console.log("length: " + $("js-active-input").length);
-	if (queryComplete) {
-		//console.log("toggle_2");
-		//$(".js-input").children().text("");
-		//$(".js-input").val("");
-		//$(".js-active").find(".collapse").children().show();
-		//hideElement(".js-input").val("");
-		//hideElement(".js-input").children().text("");
+	else if (date[2].toString().length == 2)
+		date[2] = "20" + date[2].toString();
+	return date
+}
 
-		// if the selected element is currently active, hide it
-		if ($(element).parents(".js-section").hasClass("js-active-input")) {
-			console.log("hide active");
-			//$(".collapse").hide();
-			$(".js-active-input").find(".collapse").children().show();
-			$(".js-active-input").find(".collapse").slideToggle(200);
-			$(".js-active-input").find(".cancel-input").hide();
-			$(".js-active-input").removeClass("js-active-input js-active active-section");
-			//$(".js-active").find(".cancel-input").hide();
-			//activateNextSection();
-		}
-		// if the selected element is not active, show it and hide the active one
-		else {
-			if ($(".js-active-input")[0]){
-				console.log("hide active, show inactive");
-				
-				// hide active input
-				$(".js-active-input").find(".collapse").children().show();
-				$(".js-active-input").find(".cancel-input").hide();
-				$(".js-active-input").find(".collapse").slideToggle(200);
-				$(".js-active-input").removeClass("js-active-input js-active active-section");
-				
-				// show selected input
-				$(element).parents(".js-section").addClass("js-active-input js-active active-section");
-				//$(".collapse").hide();
-				if($(element).find(".date")) {
-					$(".collapse").hide();
-				}
-				$(".js-active-input").find(".collapse").children().show();
-				$(".js-active-input").find(".cancel-input").show();
-				$(".js-active-input").find(".collapse").slideToggle(200);
-				$(".js-active-input").find(".input:first").focus();
-				/*
-				$(".js-section").removeClass("js-active");
-				showElement(element).parents(".js-section").addClass("js-active");
-				showElement(".js-active").children(".js-input").show();
-				$(".js-section").removeClass("active-section");
-				$(".js-active").addClass("active-section");
-				changeWorkElement("reset");
-				*/
-			}
-			// if there is no active element, show the selected one
-			else {
-				console.log("show inactive");
-				console.log($(element).find(".collapse"));
-			
-				//$(element).find(".collapse").children().show();
-				//$(element).find(".collapse").slideToggle(200);
-				$(element).parents(".js-section").addClass("js-active-input js-active active-section");
-				//$(".collapse").hide();
-				if($(element).find(".date")) {
-					$(".collapse").hide();
-				}
-				$(".js-active-input").find(".collapse").children().show();
-				$(".js-active-input").find(".cancel-input").show();
-				//$(".js-active-input").find().show();
-				//console.log($(".js-active-input").find(".collapse").children());
-				$(".js-active-input").find(".collapse").slideToggle(200);
-				$(".js-active-input").find(".input:first").focus();
-			}
-		}
-	}
+function onEnterEvent() {
+	$("#date-input").keydown(function(event){
+		if(event.keyCode == 13)
+			submitTime();	
+	});
+	$("#time-input").keydown(function(event){
+		if(event.keyCode == 13)
+			submitTime();
+	});
 }
+
 function hideKeyboard() {
 	$(document.activeElement).filter(':input:focus').blur();
 }
 
+// Eliminates 300ms click delay on mobile 
 function removeClickDelay() {
-	// Eliminates 300ms click delay on mobile 
 	window.addEventListener('load', function() {
 			new FastClick(document.body);
 			}, false);
