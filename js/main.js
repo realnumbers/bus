a72362624ef12be7dbcedb49acad932f5f745a3f
Julian Sparber <julian@sparber.net>
2014-04-05 23:47:15 +0200

    [fix] fixed privat session bug

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 var lang = "it";
 var History = window.History;
 var matchingBusstops = new Array(5);
+// init all stuff
 $(document).on("requestComplete", requestComplete);
-initApp();
+$(document).on("busstopsLoaded", initApp);
+
+checkStorage();
+loadBusstopsList();
 
 function initApp() {
 	selectLanguage();
@@ -69,10 +73,56 @@ History.Adapter.bind(window,'statechange',function() {
 	if (state.data.detail == 0)
 		changeToSearch();
 	else if (state.data.detail > 0)
-		changeToDetails(state.data.detail);	
+		startRequest();
+	
 });
 
 function selectLanguage() {
 	if (navigator.language === "de")
 		lang = "de";
+}
+
+// creats localStorage by using cookies for IE 6,7 if it doesnt exist
+function checkStorage() {
+	if (!window.localStorage) {
+	window.localStorage = {
+		getItem: function (sKey) {
+			if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
+				return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+		},
+		key: function (nKeyId) {
+				return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
+		},
+		setItem: function (sKey, sValue) {
+			if(!sKey) { return; }
+				 document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
+				 this.length = document.cookie.match(/\=/g).length;
+		},
+		length: 0,
+		removeItem: function (sKey) {
+			if (!sKey || !this.hasOwnProperty(sKey)) { return; }
+				document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+				this.length--;
+		},
+		hasOwnProperty: function (sKey) {
+			return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+		}
+		};
+		window.localStorage.length = (document.cookie.match(/\=/g) || window.localStorage).length;
+	}
+}
+// load busstops to localStorage if the arnt loaded
+function loadBusstopsList() {
+	if (!localStorage.busstops){
+		$.getJSON( "js/busstops.json", function(data) {
+			localStorage.setItem('busstops', JSON.stringify(data));
+			$.event.trigger({
+				type: "busstopsLoaded"
+			});
+		});
+	}
+	else
+		$.event.trigger({
+			type: "busstopsLoaded"
+		});
 }
--- js/request.js
+++ js/request.js
@@ -1,7 +1,3 @@
-// init all stuff
-checkStorage();
-loadBusstopsList();
-
 //loads the ? arguments into Status data
 function loadUrlData() {
 	if (History.getPageUrl().split("?")[1]) {
@@ -334,40 +330,4 @@ function timeString(waitTime) {
 	return waitTimeString;
 }
 
-// creats localStorage by using cookies for IE 6,7 if it doesnt exist
-function checkStorage() {
-	if (!window.localStorage) {
-	window.localStorage = {
-		getItem: function (sKey) {
-			if (!sKey || !this.hasOwnProperty(sKey)) { return null; }
-				return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
-		},
-		key: function (nKeyId) {
-				return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
-		},
-		setItem: function (sKey, sValue) {
-			if(!sKey) { return; }
-				 document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
-				 this.length = document.cookie.match(/\=/g).length;
-		},
-		length: 0,
-		removeItem: function (sKey) {
-			if (!sKey || !this.hasOwnProperty(sKey)) { return; }
-				document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
-				this.length--;
-		},
-		hasOwnProperty: function (sKey) {
-			return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
-		}
-		};
-		window.localStorage.length = (document.cookie.match(/\=/g) || window.localStorage).length;
-	}
-}
-// load busstops to localStorage if the arnt loaded
-function loadBusstopsList() {
-	if (!localStorage.busstops){
-		$.getJSON( "js/busstops.json", function(data) {
-			localStorage.setItem('busstops', JSON.stringify(data));
-		});
-	}
-}
+function Callback() {}
