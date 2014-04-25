5c1abd071f2a41589cbc9554b5b60a33868a764b
bertob <bertob@zenbook>
2014-04-25 18:15:31 +0200

    [add] file with localization for german

---
+++ js/l10n.js
@@ -0,0 +1,39 @@
+var l10n_en = {
+	"l10n-inner": {
+		"l-from": "From",
+		"l-to": "To",
+		"l-when": "When",
+		"l-no-matches": "No matching bus stops",
+		"l-more": "Load more results",
+	},
+	"l10n-plh": {
+		"lp-from": "Start...",
+		"lp-to": "Destination...",
+	},
+	"l10n-alt": {
+		"la-menu": "Menu",
+		"la-cancel": "Reset query",
+		"la-hide-input": "Hide input",
+		"la-back": "Back",
+	}
+};
+
+var l10n_de = {
+	"l10n-inner": {
+		"l-from": "Von",
+		"l-to": "Nach",
+		"l-when": "Wann",
+		"l-no-matches": "Keine passenden Haltestellen",
+		"l-more": "Mehr Resultate laden",
+	},
+	"l10n-plh": {
+		"lp-from": "Start...",
+		"lp-to": "Ziel...",
+	},
+	"l10n-alt": {
+		"la-menu": "Menü",
+		"la-cancel": "Anfrage zurücksetzen",
+		"la-hide-input": "Eingabefeld verstecken",
+		"la-back": "Zurück",
+	}
+};
